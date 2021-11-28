4000e0dfbf4b88eb01818b6871fa8c852b1a8ecd
Pavel <wol1414@gmail.com>
2021-11-28 16:36:54 +0300

    edit , delete webp avif

--- gulpfile.js
+++ gulpfile.js
@@ -206,16 +206,16 @@ function img() {
         .pipe(sharpResponsive({
             formats: [
                 // 2x
-                { pngOptions: { quality: 80, progressive: true }, rename: { dirname: path2}},
-                { jpegOptions: { quality: 80, progressive: true }, rename: { dirname: path2 } },
-                { format: "webp", rename: { dirname: `${path2}webp/` } },
-                { format: "avif", rename: { dirname: `${path2}avif/` } },
+                { pngOptions: { quality: 90, progressive: true }, rename: { dirname: path2}},
+                { jpegOptions: { quality: 90, progressive: true }, rename: { dirname: path2 } },
+                // { format: "webp", webpOptions: { quality: 100, progressive: true }, rename: { dirname: `${path2}webp/` } },
+                // { format: "avif", avifOptions: { quality: 100, progressive: true }, rename: { dirname: `${path2}avif/` } },
                 
                 // 1x
                 {width: w50, pngOptions: { quality: 80, progressive: true }, rename: { dirname: path1}},
                 {width: w50, jpegOptions: { quality: 80, progressive: true }, rename: { dirname: path1 } },
-                {width: w50, format: "webp", rename: { dirname: `${path1}webp/` } },
-                {width: w50, format: "avif", rename: { dirname: `${path1}avif/` } },
+                // {width: w50, webpOptions: { quality: 100, progressive: true }, format: "webp", rename: { dirname: `${path1}webp/` } },
+                // { width: w50, avifOptions: { quality: 100, progressive: true }, format: "avif", rename: { dirname: `${path1}avif/` } },
                 
             ]
         }))
